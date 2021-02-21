500473f100e91ef5994e2dedb9b5511d448edd73
jboullion <jboullion83@gmail.com>
2021-02-21 00:10:11 -0600

    Move Firebase into it's own config file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
 
-// https://firebase.google.com/docs/auth/web/google-signin
-// https://medium.com/@anas.mammeri/vue-2-firebase-how-to-add-firebase-social-sign-in-into-your-vue-application-21f341bbf1b
-import firebase from "firebase/app";
-// import "firebase/analytics";
+import "./firebase";
 
 //https://github.com/Maronato/vue-toastification/tree/next
 import Toast from "vue-toastification";
@@ -13,6 +10,8 @@ import "vue-toastification/dist/index.css";
 import store from './store/index';
 
 import router from './router';
+
+import "./firebase";
 
 import App from './App';
 
@@ -24,20 +23,6 @@ const toastOptions = {
 	maxToasts: 20,
 	newestOnTop: true
 };
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDkFuySyVdIFw7yrrjCU5VLT5Z1mYH24po",
-    authDomain: "vuetube-305505.firebaseapp.com",
-    projectId: "vuetube-305505",
-    storageBucket: "vuetube-305505.appspot.com",
-    messagingSenderId: "379575362506",
-    appId: "1:379575362506:web:970f65fb6c4c912f9aa533",
-    measurementId: "G-X9081RKYRM"
-  };
-
-firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
 
 const app = createApp(App);
 app.use(store);
